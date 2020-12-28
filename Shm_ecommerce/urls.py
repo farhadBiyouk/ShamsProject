a14dcf81bcc9149742df516585f86b94b7fdd4fc
farhadBiyouk <57154078+farhadBiyouk@users.noreply.github.com>
2020-12-28 16:41:03 +0330

    Update urls.py
    added about page to url

--- Shm_ecommerce/urls.py
+++ Shm_ecommerce/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, preview, products
+from .views import index, preview, products, about
 
 urlpatterns = [
     path('', index, name="homePage"),
     path('preview/', preview, name="previewPage"),
-    path('products/', products, name="productsPage")
+    path('products/', products, name="productsPage"),
+    path('about/', about, name="aboutPage")
 ]
