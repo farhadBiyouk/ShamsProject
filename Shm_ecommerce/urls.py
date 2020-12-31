86a39e539fc257ec08374d0f7320b33949912a27
farhadBiyouk <57154078+farhadBiyouk@users.noreply.github.com>
2020-12-31 18:01:58 +0330

    Update urls.py
    Added url contact

--- Shm_ecommerce/urls.py
+++ Shm_ecommerce/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import index, preview, products, about
+from .views import index, preview, products, about, contact
 
 urlpatterns = [
     path('', index, name="homePage"),
     path('preview/', preview, name="previewPage"),
     path('products/', products, name="productsPage"),
-    path('about/', about, name="aboutPage")
+    path('about/', about, name="aboutPage"),
+    path('contact-us, contact, name="contactPage")
 ]
